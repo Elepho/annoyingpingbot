eb2657e3498ab1fa641cc4c7a541cc8048180b16
Elepho <thomas.croson@gmail.com>
2019-09-30 15:47:41 -0400

    added rudimentary subreddit image fetching

--- bot.js
+++ bot.js
@@ -5,6 +5,7 @@ const ids = require('./ids.json');
 const schedule = require('node-schedule');
 const ffmpeg = require('ffmpeg');
 const insults = require('./insults.js');
+const snekfetch = require('snekfetch');
 
 // Configure logger settings, note the logger timestamps are in utc + 0
 const logger = createLogger({
@@ -18,40 +19,17 @@ const logger = createLogger({
     });
 logger.level = 'debug';
 
-function schedulePing() {
-	var randHour = Math.floor(Math.random() * (18 - 12) + 12);
-	var randMin = Math.floor(Math.random() * 59);
-
-	// schedule ping
-	if (typeof dailyjob == "undefined") {
-		var dailyjob = schedule.scheduleJob({hour: randHour, minute: randMin}, () => {
-			client.guilds.get(ids.guildid).channels.get(ids.channelid).send('owo *notices <@' + ids.userid + '>*')
-				.then(logger.info('Sent ping to poser'))
-				.catch(console.error);
-		});
-		logger.info('The message today will be at ');
-		logger.info(dailyjob.nextInvocation());
-
-	} else {
-
-		dailyjob.reschedule({hour: randHour, minute: randMin});
-		logger.info('The message today will be at ');
-		logger.info(dailyjob.nextInvocation());
-	}
-}
-
 // Initialize Discord client
-const client = new Discord.Client();
+var client = new Discord.Client();
 client.login(auth.token);
 client.on('ready', () => {
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(client.user.username + ' - (' + client.user.id + ')');
-    client.user.setPresence({ game: { name: 'owo *notices poser*', type: 0 } });
+    client.user.setPresence({ game: { name: 'god', type: 0 } });
 
 	// if bot is started between 1 am and noon, schedule a ping
 	var date = new Date();
-	
 	if (date.getHours() > 0 && date.getHours() < 12) {
 		schedulePing();
 	}
@@ -71,16 +49,12 @@ client.on('message', message => {
 			case '🏓':
 			case 'ping':
 				logger.info(message.author.username + ' pinged');
-				message.channel.send('Pong!');
-				break;
-			case 'annoy':
-				logger.info(message.author.username + ' played themself');
-				message.channel.send(message.author.toString() + ' is a ninny');
+				message.channel.send('Beep beep, ' + message.author.username);
 				break;
 			case 'test':
 				logger.info(message.author.username + ' did a command test');
-				var currentchannel = client.guilds.get(ids.guildid).channels.get(ids.testchannelid);
-				currentchannel.send('This channel is '  + currentchannel.name);
+				var temp = message.channel.send('This channel is '  + message.channel.name)
+					.then((msg) => {msg.delete(1000)});
 				break;
 			case 'connect':
 				var voiceChannel = message.member.voiceChannel;
@@ -95,6 +69,25 @@ client.on('message', message => {
 				logger.info(message.author.username + ' made the bot leave ' + client.voiceConnections.first().channel.name);
 				client.voiceConnections.first().disconnect();
 				break;
+			case 'curse':
+				curseFetch(message);
+				logger.info(message.author.username + ' cursed themself');
+				break;
+			case 'subfetch':
+				var url = subImageFetch(message, args[0])
+					.then(post => {
+						if (post.length > 0) { 
+							logger.info(message.author.username + ' fetched an image from /r/' + args[0]);
+							message.channel.send({
+								files:
+								[post]
+							})
+						} else {
+							logger.info(message.author.username + ' failed to fetch an image from /r/' + args[0]);
+							message.channel.send('no posts found, fuck off')
+						}
+					});
+				break;
             // Just add any case commands if you want to..
 		}
 	} else if (message.content.includes('<@621857544779988992>')) {
@@ -102,5 +95,63 @@ client.on('message', message => {
 	}
 });
 
+function schedulePing() {
+	var randHour = Math.floor(Math.random() * (18 - 12) + 12);
+	var randMin = Math.floor(Math.random() * 59);
 
-	
+	// schedule ping
+	if (typeof dailyjob == "undefined") {
+		var dailyjob = schedule.scheduleJob({hour: randHour, minute: randMin}, () => {
+			client.guilds.get(ids.guildid).channels.get(ids.channelid).send("<@" + ids.userid + ">", {
+				files: [{
+					attachment: 'unnamed.jpg',
+					name: 'unnamed.jpg'
+				}]
+			})
+				.then(logger.info('Sent ping to poser'))
+				.catch(console.error);
+		});
+		logger.info('The message today will be at ');
+		logger.info(dailyjob.nextInvocation());
+
+	} else {
+
+		dailyjob.reschedule({hour: randHour, minute: randMin});
+		logger.info('The message today will be at ');
+		logger.info(dailyjob.nextInvocation());
+	}
+}
+
+async function subImageFetch(message, sub) {
+	try {
+        const { body } = await snekfetch
+            .get('https://api.reddit.com/r/' + sub + '.json?sort=top&t=week&limit=100')
+            .query({ limit: 800 });
+        var posts = message.channel.nsfw ? body.data.children : body.data.children.filter(post => !post.data.over_18);
+		posts = posts.filter(post => !post.data.selftext.length);
+		posts = posts.filter(post => (post.data.url.endsWith('.jpg') || post.data.url.endsWith('.png') || post.data.url.endsWith('.gif')));
+        if (!posts.length) return '';
+        const randomnumber = Math.floor(Math.random() * posts.length);
+        return posts[randomnumber].data.url;
+    } catch (err) {
+		return '';
+    }
+}
+
+async function curseFetch(message) {
+	try {
+        const { body } = await snekfetch
+            .get('https://api.reddit.com/r/cursedimages.json?sort=top&t=week&limit=100')
+            .query({ limit: 800 });
+        var posts = message.channel.nsfw ? body.data.children : body.data.children.filter(post => !post.data.over_18);
+		posts.filter(post => !post.data.selftext.length);
+        if (!posts.length) return message.channel.send('It seems we are out of cursed images, Try again later.');
+        const randomnumber = Math.floor(Math.random() * posts.length);
+        message.channel.send({
+			files:
+			[posts[randomnumber].data.url]
+		});
+    } catch (err) {
+        return console.log(err);
+    }
+}
